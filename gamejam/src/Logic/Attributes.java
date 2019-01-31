f558d214acd5f264e5b045f6ccdc8e61a24a5054
Manuel Rickli <manuel.rickli@stud.unibas.ch>
2019-01-31 18:07:45 +0100

    component structure

--- gamejam/src/Logic/Attributes.java
+++ gamejam/src/Logic/Attributes.java
@@ -1,4 +1,30 @@
 package Logic;
 
 public enum Attributes {
+    SPEED(0),
+    HEALTH(1),
+    COLOR(2),
+    LOOT(3),
+    WIDTH(4),
+    SLOTS(5),
+    DAMAGE(6),
+    RANGE(7),
+    COOLDOWN(8),
+    SLOTSIZE(9),
+    ANGLE(10),
+    ARMOR(11),
+    ACCELERATION(12),
+    CATEGORY(13),
+    IMAGE(14),
+    HEIGHT(15);
+
+    private final int att;
+
+    private Attributes(int att) {
+        this.att = att;
+    }
+
+    public int valueOf() {
+        return this.att;
+    }
 }
---
+++ gamejam/src/Logic/Categories.java
@@ -0,0 +1,20 @@
+package Logic;
+
+public enum Categories {
+    SHIP(0),
+    CANON(1),
+    TURRET(2),
+    TREASURE(3),
+    ARMOR(4),
+    ACCELERATOR(5);
+
+    private final int cat;
+
+    private Categories(int cat) {
+        this.cat = cat;
+    }
+
+    public int valueOf() {
+        return this.cat;
+    }
+}

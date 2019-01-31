3afa9e93220626eaed5389f57e6d4e0a32fef669
MickeyMouse <michael@pluess.net>
2019-01-31 16:03:27 +0100

    Created first graphics

--- gamejam/src/graphics/Window.java
+++ gamejam/src/graphics/Window.java
@@ -1,4 +1,28 @@
 package graphics;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Window {
+
+	private int width, height;
+	private Canvas canvas;
+	private JFrame frame;
+
+	public Window () {
+		width = 800;
+		height = 600;
+
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame("LustigiGameJam");
+		frame.add(canvas = new Screen(width, height));
+		frame.setLayout(null);
+		frame.setSize(width, height);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 }

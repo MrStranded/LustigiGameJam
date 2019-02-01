50df1dcc7a0c5bab8b5c4bf65762bce9f3211aa0
Manuel Rickli <manuel.rickli@stud.unibas.ch>
2019-02-01 20:56:13 +0100

    do

--- gamejam/src/Logic/WorldState.java
+++ gamejam/src/Logic/WorldState.java
@@ -6,129 +6,142 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class WorldState {
 
-	private final int tileSize = 40;
-	private final int mapSize = 32;
+    private final int tileSize = 40;
+    private final int mapSize = 32;
 
-	private int[][] map;
-	private int size;
+    private int[][] map;
+    private int size;
 
-	public int userId = 0;
-	public String userName = "-";
-	public String ip = "127.0.0.1";
+    public int userId = 0;
+    public String userName = "-";
+    public String ip = "127.0.0.1";
 
-	private ConcurrentLinkedDeque<Player> players;
-	private ConcurrentLinkedDeque<Component> units;
-	private ConcurrentLinkedDeque<String> chatMessages;
+    private ConcurrentLinkedDeque<Player> players;
+    private ConcurrentLinkedDeque<Component> units;
+    private ConcurrentLinkedDeque<String> chatMessages;
 
-	private HashMap<String, String> ipNameTuples;
+    private HashMap<String, String> ipNameTuples;
+    private HashMap<Integer, Integer[]> playerControlTuples;
 
-	public HashMap<String, String> getIpNameTuples() {
-		return ipNameTuples;
-	}
+    public HashMap<String, String> getIpNameTuples() {
+        return ipNameTuples;
+    }
 
-	public void setIpNameTuples(HashMap<String, String> ipNameTuples) {
-		this.ipNameTuples = ipNameTuples;
-	}
+    public void setIpNameTuples(HashMap<String, String> ipNameTuples) {
+        this.ipNameTuples = ipNameTuples;
+    }
 
-	public WorldState() {
-		players = new ConcurrentLinkedDeque<Player>();
-		units = new ConcurrentLinkedDeque<Component>();
-		chatMessages = new ConcurrentLinkedDeque<String>();
+    public WorldState() {
+        players = new ConcurrentLinkedDeque<Player>();
+        units = new ConcurrentLinkedDeque<Component>();
+        chatMessages = new ConcurrentLinkedDeque<String>();
 
-		createTestMap();
-	}
+        createTestMap();
+    }
 
-	public void createTestMap() {
-		size = mapSize;
-		map = new int[size][size];
+    public void createTestMap() {
+        size = mapSize;
+        map = new int[size][size];
 
-		for (int x=0; x<size; x++) {
-			for (int y=0; y<size; y++) {
-				map[x][y] = (int) (Math.random()*3d);
-			}
-		}
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                map[x][y] = (int) (Math.random() * 3d);
+            }
+        }
 
-		units = new ConcurrentLinkedDeque<>();
-	}
+        units = new ConcurrentLinkedDeque<>();
+    }
 
-	public void addUnit(Component component) {
-		units.add(component);
-	}
+    public void addUnit(Component component) {
+        units.add(component);
+    }
 
-	public void addPlayer(Player player) {
-		players.add(player);
-	}
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
 
-	public int getTileSize() {
-		return tileSize;
-	}
+    public int getTileSize() {
+        return tileSize;
+    }
 
-	public int[][] getMap() {
-		return map;
-	}
+    public int[][] getMap() {
+        return map;
+    }
 
-	public void setMap(int[][] map) {
-		this.map = map;
-	}
+    public void setMap(int[][] map) {
+        this.map = map;
+    }
 
-	public int getSize() {
-		return size;
-	}
+    public int getSize() {
+        return size;
+    }
 
-	public ConcurrentLinkedDeque<Component> getUnits() {
-		return units;
-	}
+    public ConcurrentLinkedDeque<Component> getUnits() {
+        return units;
+    }
 
-	public void removeUnit(Component unit) {
-		units.remove(unit);
-	}
+    public void removeUnit(Component unit) {
+        units.remove(unit);
+    }
 
-	public Component getUnit(int id) {
-		for (Component component : units) {
-			if (component.getId() == id) {
-				return component;
-			}
-		}
-		return null;
-	}
+    public Component getUnit(int id) {
+        for (Component component : units) {
+            if (component.getId() == id) {
+                return component;
+            }
+        }
+        return null;
+    }
 
-	public ConcurrentLinkedDeque<Player> getPlayers() {
-		return players;
-	}
+    public ConcurrentLinkedDeque<Player> getPlayers() {
+        return players;
+    }
 
-	public Player getPlayer(int id) {
-		for (Player player : players) {
-			if (player.getId() == id) {
-				return player;
-			}
-		}
-		return null;
-	}
+    public Player getPlayer(int id) {
+        for (Player player : players) {
+            if (player.getId() == id) {
+                return player;
+            }
+        }
+        return null;
+    }
 
-	public void setPlayers(ConcurrentLinkedDeque<Player> players) {
-		this.players = players;
-	}
+    public void setPlayers(ConcurrentLinkedDeque<Player> players) {
+        this.players = players;
+    }
 
-	public void setUnits(ConcurrentLinkedDeque<Component> units) {
-		this.units = units;
-	}
+    public void setUnits(ConcurrentLinkedDeque<Component> units) {
+        this.units = units;
+    }
 
-	public void addChatMessage(String msg) {
-		chatMessages.add(msg);
-		if (chatMessages.size() > 5) {
-			chatMessages.pop();
-		}
-	}
+    public void addChatMessage(String msg) {
+        chatMessages.add(msg);
+        if (chatMessages.size() > 5) {
+            chatMessages.pop();
+        }
+    }
 
-	public ConcurrentLinkedDeque<String> getChatMessages() {
-		return chatMessages;
-	}
+    public ConcurrentLinkedDeque<String> getChatMessages() {
+        return chatMessages;
+    }
 
-	public int getUserId() {
-		return userId;
-	}
+    public int getUserId() {
+        return userId;
+    }
 
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public HashMap<Integer, Integer[]> getControls() {
+        return playerControlTuples;
+    }
+
+    public void addControl(int playerId, Integer[] controls) {
+        playerControlTuples.put(playerId, controls);
+    }
+
+//    public void clearControls() {
+//        playerControlTuples.clear();
+//    }
 }
